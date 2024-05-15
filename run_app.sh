4a5e36e1025d38d0f47dadf286453a5532dbb4aa
g0tMandlik <128036478+g0tMandlik@users.noreply.github.com>
2024-05-15 11:20:21 +0530

    Update run_app.sh

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -cp /var/lib/jenkins/workspace/Java/target/classes/com.houari.Calculator
+java -cp /var/lib/jenkins/workspace/Java/target/classes com.houarizegai.calculator.App
 java -jar /var/lib/jenkins/workspace/Java/target/Calculator-1.0-SNAPSHOT.jar
